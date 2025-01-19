d35731d6850e99b8b5a1fd834dca5aae6bb931da
BLUETOXINE <133049188+bluetox@users.noreply.github.com>
2025-01-19 21:21:38 +0100

    Delete app directory

--- app/static/js/cdn/crypto.js
+++
@@ -1,3 +0,0 @@
-/* esm.sh - esbuild bundle(@noble/hashes@1.3.3/crypto) es2021 production */
-var o=typeof globalThis=="object"&&"crypto"in globalThis?globalThis.crypto:void 0;export{o as crypto};
-//# sourceMappingURL=crypto.js.map
